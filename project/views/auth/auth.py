76a0fe611c390903f8a4b31858b93eb7b24c30f0
user <ekl18062003@yandex.ru>
2023-01-06 14:28:31 +0300

    change

--- project/views/auth/auth.py
+++ project/views/auth/auth.py
@@ -11,19 +11,19 @@ class RegisterView(Resource):
     def post(self):
         data = request.json
         if data.get('email') and data.get('password'):
-            return user_service.create_user(data.get('email')),data.get('password'), 201
+            return user_service.create_user(data.get('email'),data.get('password')), 201
         else:
             return "Чего-то не хватает", 401
 
 
-@api.route('/login /')
+@api.route('/login/')
 class LoginView(Resource):
     @api.response(404, 'Not Found')
     #@api.marshal_with(user, code=200, description='OK')
     def post(self):
         data = request.json
         if data.get('email') and data.get('password'):
-            return user_service.check(data.get('email')), data.get('password'), 201
+            return user_service.check(data.get('email'), data.get('password')), 201
         else:
             return "Чего-то не хватает", 401
 
